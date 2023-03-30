bf52c70660f7ce604ee69ff83861afed7ce256e1
teis-ravn-jonsen <tera41930@edu.ucl.dk>
2023-03-30 16:10:58 +0200

    works pretty much just need pages and som other details I think

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
+// App.tsx
 import { render } from 'solid-js/web';
 import { Header } from '../Header/Header';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { MainContent } from '../MainContent/MainContent';
 import { Footer } from '../Footer/Footer';
-import { RightSidebar } from '../RightSidebar/RightSidebar';
+import { TableOfContents } from '../TableOfContents/TableOfContents';
 import style from './App.module.scss';
 
 export function App(props: { children?: any }) {
@@ -14,11 +15,10 @@ export function App(props: { children?: any }) {
                 <div class={style.container}>
                     <Sidebar class={style.sidebar} />
                     <MainContent class={style.mainContent}>{props.children}</MainContent>
-                    {/* <RightSidebar class={style.rightSidebar} /> */}
+                    <TableOfContents class={`${style.tableOfContents}`} />
                 </div>
                 <Footer />
             </div>
         </>
     );
 }
-
