7f0b3c9526f23198f1ce23f8626ab9fb19ac4149
Birk Skyum <birk.skyum@pm.me>
2023-03-30 04:38:16 +0200

    fix header

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import style from './sidebar.module.scss'
 import './sidebar.css'
-import { A, useNavigate } from 'solid-start'
+import { A} from 'solid-start'
 import { pages } from '~/pages';
 
 interface SidebarProps {
@@ -9,12 +9,9 @@ interface SidebarProps {
 
 export function Sidebar(props: SidebarProps) {
 
-    const navigate = useNavigate();
+    
     return (
         <aside class={`${style.sidebar} ${props.class}`}>
-            <h1 class={style.header} onclick={()=>{navigate('/')}}>MapLibre Style Spec</h1>
-            <hr />
-            
             <div class={style.navItems}>
                 <ul>
                     {pages.map((page) => (
