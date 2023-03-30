231af9fddc89caebee1946132da4005b36d954c4
Birk Skyum <birk.skyum@pm.me>
2023-03-30 04:49:56 +0200

    cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,3 @@
-// import solid from "solid-start/vite";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   plugins: [solid({
-//     ssr: true,
-//   })],
-// });
-
-
-
-
 import solid from "solid-start/vite";
 import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
